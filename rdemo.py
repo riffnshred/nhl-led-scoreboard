f108641ae749eaf9aee2e163f9342f49f39371dd
riffnshred <j.joannisse@gmail.com>
2019-05-13 08:05:18 -0400

    Update rdemo.py

--- rdemo.py
+++ rdemo.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date=2018-01-09', data={'key': 'value'})
+r = requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date=2018-01-09')
 
 data = r.json()
 #games = data['dates']['totalGames']
