28a0ecb5ec95780b9a13caf06cb28f36bcce30fc
riffnshred <j.joannisse@gmail.com>
2019-10-25 20:48:00 -0400

    Fix issue #1 .
    Fix the condition where if the user dont put any team's ID in arguments it would not pick up the default ID from config

--- data/scoreboard_config.py
+++ data/scoreboard_config.py
@@ -6,13 +6,15 @@ import os
 class ScoreboardConfig:
     def __init__(self, filename_base, args):
         json = self.__get_config(filename_base)
-
         # Misc config options
         self.end_of_day = json["end_of_day"]
         self.debug = json["debug"]
 
         # config options from arguments. If the argument was passed, use it's value, else use the one from config file.
-        self.fav_team_id = args.fav_team if True else json['fav_team_id']
+        if args.fav_team:
+            self.fav_team_id = args.fav_team
+        else:
+            self.fav_team_id = json['fav_team_id']
 
     def read_json(self, filename):
         # Find and return a json file
