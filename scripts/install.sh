e7dbe6568bb462da3cb380e39c35c76cf3ef6bd5
Josh Kay <josh.m.kay@gmail.com>
2020-02-06 23:56:37 -0500

    Updated install script / makefile to support new folder structure.  Fixed matrix submodule.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+
+# Make script work regardless of where it is run from
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "../$parent_path"
+
+# Pull submodule and ignore changes from script
 git submodule update --init --recursive
 git config submodule.matrix.ignore all
-cd matrix || exit
+
+cd submodules/matrix || exit
 echo "Running rgbmatrix installation..."
 sudo apt-get update && sudo apt-get install python3-dev python3-pillow -y
 make build-python PYTHON=$(which python3)
 sudo make install-python PYTHON=$(which python3)
 cd bindings
 sudo pip3 install -e python/
-cd ../../
+
+cd ../../../
+
 echo "Installing required dependencies. This may take some time (10-20 minutes-ish)..."
 # git reset --hard
 # git checkout master
@@ -16,6 +25,7 @@ echo "Installing required dependencies. This may take some time (10-20 minutes-i
 # git pull
 sudo pip3 install requests 
 sudo pip3 install geocoder python_tsl2591 ephem
+
 make
 echo "If you didn't see any errors above, everything should be installed!"
 echo "Installation complete! Play around with the examples in matrix/bindings/python/samples to make sure your matrix is working."
