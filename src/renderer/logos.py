773ea3a4e8bb55ef374288fe3eb962d918f37c09
Josh Kay <josh.m.kay@gmail.com>
2020-02-08 17:48:37 -0500

    SVG logos are auto downloaded, converted, and saved. Logos are positioned automatically with a given offset.

--- src/renderer/logos.py
+++ src/renderer/logos.py
@@ -1,20 +1,71 @@
 from PIL import Image
 from utils import get_file
+from images.image_helper import ImageHelper
+import os
+import errno
+
+USE_SVGS = True
+AUTO_POSITION = True
+AUTO_POSITION_OFFSET = 20
+
+PATH = 'assets/logos'
+LOGO_NAME = 'teams-current-primary-light'
+LOGO_URL = 'https://www-league.nhlstatic.com/images/logos/{}/{}.svg'
 
 class TeamLogos:
     def __init__(self, matrix, layout, team, gameLocation):
         self.matrix = matrix
         self.layout = layout
-        self.team = team
         self.gameLocation = gameLocation
 
-    def render(self):
+        self.load(team)
+
+    def get_path(self, team):
+        if (USE_SVGS):
+            return get_file('{}/{}/{}.png'.format(PATH, team.abbrev, LOGO_NAME))
+        
+        return get_file('{}/{}.png'.format(PATH, team.abbrev))
+
+    def load(self, team):
         # Get the on-screen position of both logos
-        logo_pos = self.layout.get_scoreboard_logo_coord(self.team.id)[self.gameLocation]
+        self.location = self.layout.get_scoreboard_logo_coord(
+            team.id
+        )[self.gameLocation]
+        
+        filename = self.get_path(team)
 
-        # Open the logo image file
-        team_logo = Image.open(get_file('assets/logos/{}.png'.format(self.team.abbrev)))
+        try:
+            self.logo = Image.open(filename)
+        except FileNotFoundError:
+            if (USE_SVGS):
+                self.save_image(filename, team)
+       
+    def save_image(self, filename, team):
+        if not os.path.exists(os.path.dirname(filename)):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+                
+        self.logo = ImageHelper.image_from_svg(
+            LOGO_URL.format(LOGO_NAME, team.id)
+        )
+        self.logo.thumbnail((64, 32))
+        #self.logo = self.logo.transpose(method=Image.FLIP_LEFT_RIGHT)
+        self.logo.save(filename)
 
+    def render(self):
         # Put the images on the canvas
-        self.matrix.draw_image((logo_pos["x"], logo_pos["y"]), team_logo)
+        if (AUTO_POSITION):
+            x = 0
+            
+            image_location = "right" if self.gameLocation == "home" else "left"
+            if image_location == 'right':
+                x = self.logo.width - AUTO_POSITION_OFFSET
+            elif image_location == 'left':
+                x = AUTO_POSITION_OFFSET - self.logo.width
 
+            self.matrix.draw_image((x, 0), self.logo, image_location)
+        else:
+            self.matrix.draw_image((self.location["x"], self.location["y"]), self.logo)
