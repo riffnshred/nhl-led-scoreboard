4b3da2366d96a232c58a808f259c623081079b0b
riffnshred <j.joannisse@gmail.com>
2020-02-08 08:52:28 -0500

    Preferred standings done and standings fixed
    now does not redraw the standing at every frame rate. CPU back to stable

--- src/boards/standings.py
+++ src/boards/standings.py
@@ -18,12 +18,46 @@ class Standings:
         if self.data.config.preferred_standings_only:
             if type == 'conference':
                 print("conf1")
-                conf = getattr(self.data.standings.by_conference, self.data.config.preferred_conference)
-                
+                conference = self.data.config.preferred_conference
+                records = getattr(self.data.standings.by_conference, conference)
+                # calculate the image height
+                im_height = (len(records) + 1) * 7
+                # Increment to move image up
+                i = 0
+                image = draw_records(self.data, conference, records, im_height, self.matrix.width)
+                self.matrix.draw_image((0, i), image)
+                self.matrix.render()
+                sleep(5)
+                # Move the image up until we hit the bottom.
+                while i > -(im_height - self.matrix.height):
+                    i -= 1
+                    self.matrix.draw_image((0, i), image)
+                    self.matrix.render()
+                    sleep(0.2)
+                # Show the bottom before we change to the next table.
+                sleep(5)
+
             elif type == 'division':
                 print("div1")
-                div = getattr(self.data.standings.by_division, self.data.config.preferred_divisions)
-                print(div)
+                division = self.data.config.preferred_divisions
+                records = getattr(self.data.standings.by_division, division)
+                # calculate the image height
+                im_height = (len(records) + 1) * 7
+                # Increment to move image up
+                i = 0
+                image = draw_records(self.data, division, records, im_height, self.matrix.width)
+                self.matrix.draw_image((0, i), image)
+                self.matrix.render()
+                sleep(5)
+
+                # Move the image up until we hit the bottom.
+                while i > -(im_height - self.matrix.height):
+                    i -= 1
+                    self.matrix.draw_image((0, i), image)
+                    self.matrix.render()
+                    sleep(0.2)
+                # Show the bottom before we change to the next table.
+                sleep(5)
         else:
             if type == 'conference':
                 for conference in self.conferences:
@@ -33,16 +67,15 @@ class Standings:
                     im_height = (len(records)+1) * 7
                     # Increment to move image up
                     i = 0
-                    image = draw_records(self.data, conference, records, im_height, self.matrix.width, i)
-                    self.matrix.draw_image((0, 0), image)
+                    image = draw_records(self.data, conference, records, im_height, self.matrix.width)
+                    self.matrix.draw_image((0, i), image)
                     self.matrix.render()
                     sleep(5)
 
                     # Move the image up until we hit the bottom.
                     while i > -(im_height - self.matrix.height):
                         i -= 1
-                        image = draw_records(self.data, conference, records, im_height, self.matrix.width, i)
-                        self.matrix.draw_image((0, 0), image)
+                        self.matrix.draw_image((0, i), image)
                         self.matrix.render()
                         sleep(0.2)
                     # Show the bottom before we change to the next table.
@@ -56,23 +89,22 @@ class Standings:
                     im_height = (len(records) + 1) * 7
                     # Increment to move image up
                     i = 0
-                    image = draw_records(self.data, division, records, im_height, self.matrix.width, i)
-                    self.matrix.draw_image((0, 0), image)
+                    image = draw_records(self.data, division, records, im_height, self.matrix.width)
+                    self.matrix.draw_image((0, i), image)
                     self.matrix.render()
                     sleep(5)
 
                     # Move the image up until we hit the bottom.
                     while i > -(im_height - self.matrix.height):
                         i -= 1
-                        image = draw_records (self.data, division, records, im_height, self.matrix.width, i)
-                        self.matrix.draw_image((0, 0), image)
+                        self.matrix.draw_image((0, i), image)
                         self.matrix.render()
                         sleep(0.2)
                     # Show the bottom before we change to the next table.
                     sleep(5)
 
 
-def draw_records(data, name, records, img_height, width, i):
+def draw_records(data, name, records, img_height, width):
     """
         Draw an image of a list of standing record of each team.
         :return the image
@@ -94,7 +126,7 @@ def draw_records(data, name, records, img_height, width, i):
     row_height = 7
     top = row_height - 1  # For some reason, when drawing with PIL, the first row is not 0 but -1
 
-    draw.text((1, 0 + i), name, font=layout.font)
+    draw.text((1, 0), name, font=layout.font)
     row_pos += row_height
 
     for team in records:
@@ -108,10 +140,10 @@ def draw_records(data, name, records, img_height, width, i):
         team_colors = data.config.team_colors
         bg_color = team_colors.color("{}.primary".format(team_id))
         txt_color = team_colors.color("{}.text".format(team_id))
-        draw.rectangle([0, top + row_pos + i, 12, row_pos + i], fill=(bg_color['r'], bg_color['g'], bg_color['b']))
-        draw.text((1, row_pos + i), abbev, fill=(txt_color['r'], txt_color['g'], txt_color['b']), font=layout.font)
-        draw.text((57, row_pos + i), points, font=layout.font)
-        draw.text((19, row_pos + i), "{}-{}-{}".format(wins, losses, ot), font=layout.font)
+        draw.rectangle([0, top + row_pos, 12, row_pos], fill=(bg_color['r'], bg_color['g'], bg_color['b']))
+        draw.text((1, row_pos), abbev, fill=(txt_color['r'], txt_color['g'], txt_color['b']), font=layout.font)
+        draw.text((57, row_pos), points, font=layout.font)
+        draw.text((19, row_pos), "{}-{}-{}".format(wins, losses, ot), font=layout.font)
         row_pos += row_height
 
         # message = "{}. {} - Points: {} - W: {} L: {} OT: {}".format(pos, abbev, points, wins, losses, ot)
