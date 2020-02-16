eeb74fbefb824f83ba361f3d7e0b7379c488a7cd
riffnshred <j.joannisse@gmail.com>
2020-02-16 11:48:33 -0500

    Create test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Make script work regardless of where it is run from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${DIR}/.."
+
+export PYTHON_VERSION=`python -c 'import sys; version=sys.version_info[:3];
+print("{0}.{1}.{2}".format(*version))'
