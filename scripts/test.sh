7eea24b179e68d599446574386eec38a4e06ff7a
riffnshred <j.joannisse@gmail.com>
2020-02-16 11:53:04 -0500

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,3 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}/.."
 
 export PYTHON_VERSION=`python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
+echo PYTHON_VERSION
