95798014a2b0400641b05b92ef11021db2ab66db
Joel Joannisse <j.joannisse@gmail.com>
2020-03-11 14:18:44 -0400

    Update playoff.py

--- src/nhl_api/playoff.py
+++ src/nhl_api/playoff.py
@@ -6,7 +6,28 @@ def playoff_info():
     data = nhl_api.data.get_playoff_data()
     parsed = data.json()
     playoff_data = parsed["rounds"]
+
+    default_round = parsed["defaultRound"]
     rounds = []
 
     for round in playoff_data:
-        pass
+        number = round['number']
+        name = round['names']['name']
+
+
+class PlayoffRound(object):
+    def __init__(self, data):
+        # loop through data
+        for x in data:
+            # set information as correct data type
+            try:
+                setattr(self, x, int(data[x]))
+            except ValueError:
+                try:
+                    setattr(self, x, float(data[x]))
+                except ValueError:
+                    # string if not number
+                    setattr(self, x, str(data[x]))
+            except TypeError:
+                obj = nhl_api.object.Object(data[x])
+                setattr(self, x, obj)
