fd44196b32860f043e922591a0a0e26fec2b0d03
riffnshred <j.joannisse@gmail.com>
2020-03-24 09:22:51 -0400

    fixed curr_board saving the boards_scheduled in _off_day and _intermission in boards.

--- src/boards/boards.py
+++ src/boards/boards.py
@@ -29,7 +29,7 @@ class Boards:
         bord_index = 0
         while True:
             board = getattr(self, data.config.boards_off_day[bord_index])
-            data.curr_board = data.config.boards_scheduled[bord_index]
+            data.curr_board = data.config.boards_off_day[bord_index]
 
             if data.pb_trigger:
                 debug.info('PushButton triggered....will display ' + data.config.pushbutton_state_triggered1 + ' board ' + "Overriding off_day -> " + data.config.boards_off_day[bord_index])
@@ -70,7 +70,7 @@ class Boards:
         bord_index = 0
         while True:
             board = getattr(self, data.config.boards_intermission[bord_index])
-            data.curr_board = data.config.boards_scheduled[bord_index]
+            data.curr_board = data.config.boards_intermission[bord_index]
 
             if data.pb_trigger:
                 debug.info('PushButton triggered....will display ' + data.config.pushbutton_state_triggered1 + ' board ' + "Overriding intermission -> " + data.config.boards_intermission[bord_index])
