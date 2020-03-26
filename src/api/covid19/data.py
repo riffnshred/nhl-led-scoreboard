9b8dd3acb50513423fa73f1ca9294267f761817b
ntjoumas <39501574+ntjoumas@users.noreply.github.com>
2020-03-26 17:50:44 -0400

    Update data.py

--- src/api/covid19/data.py
+++ src/api/covid19/data.py
@@ -1,7 +1,10 @@
 import requests
 import debug as debug
 
-BASE_URL = "https://corona.lmao.ninja/countries/us"
+BASE_URL = "https://corona.lmao.ninja/"
+WW_URL = BASE_URL + 'all'
+COUNTRY_URL = BASE_URL + 'countries'
+US_STATE_URL = BASE_URL + 'states'
 
 REQUEST_TIMEOUT = 5
 TIMEOUT_TESTING = 0.001  # TO DELETE
@@ -14,8 +17,17 @@ class Data:
     def get_all(self):
         try:
             debug.log("grabbing all data from API")
-            data = requests.get(BASE_URL)
-            self.all = data.json()
+            data = requests.get(WW_URL)
+            self.ww = data.json()
+            data = requests.get(COUNTRY_URL)
+            self.country_data = data.json()
+            self.countrydict = {}
+            for country in self.country_data:
+                 self.countrydict[country["country"]] = country 
+            data = requests.get(US_STATE_URL)
+            self.us_state = data.json()
+            self.us_state_dict = {}
+            for state in self.us_state:
+                 self.us_state_dict[state["state"]] = state
         except requests.exceptions.RequestException as e:
             raise ValueError(e)
-
