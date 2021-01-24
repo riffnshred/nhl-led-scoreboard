15459379831749837f87914a82a15283837fbcfd
riffnshred <j.joannisse@gmail.com>
2021-01-24 18:54:41 -0500

    added fix for logo ownership

--- src/renderer/logos.py
+++ src/renderer/logos.py
@@ -2,23 +2,26 @@ from PIL import Image
 from utils import get_file
 from images.image_helper import ImageHelper
 import os
+import pwd
+import grp
 import errno
 from utils import round_normal
 
+uid = int(os.stat("./VERSION").st_uid)
+gid = int(os.stat("./VERSION").st_uid)
+
 PATH = 'assets/logos'
-LOCAL_LOGO_URL = PATH+'/_local/{}_{}.svg'
 LOGO_URL = 'https://assets.nhle.com/logos/nhl/svg/{}_{}.svg'
 
 class LogoRenderer:
     def __init__(self, matrix, config, element_layout, team_abbrev, board, gameLocation=None):
         self.matrix = matrix
 
-        self.logo_variant = config.config.logos.get_team_logo(team_abbrev)
+        self.logo_name = config.config.logos.get_team_logo(team_abbrev)
         self.layout = config.config.layout.get_scoreboard_logo(
             team_abbrev, 
             board, 
-            gameLocation,
-            self.logo_variant
+            gameLocation
         )
         
         self.element_layout = element_layout
@@ -34,7 +37,7 @@ class LogoRenderer:
     def get_path(self, team_abbrev):
         size = self.get_size()
         return get_file('{}/{}/{}/{}x{}.png'.format(
-            PATH, team_abbrev, self.logo_variant, 
+            PATH, team_abbrev, self.logo_name, 
             size[0], size[1]
         ))
 
@@ -65,20 +68,26 @@ class LogoRenderer:
         if not os.path.exists(os.path.dirname(filename)):
             try:
                 os.makedirs(os.path.dirname(filename))
+                self.change_ownership(team_abbrev)
             except OSError as exc: # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
-        try:        
-            self.logo = ImageHelper.image_from_svg(
-                LOGO_URL.format(team_abbrev, self.logo_variant)
-            )
-        except:
-            self.logo = ImageHelper.image_from_svg(
-                LOCAL_LOGO_URL.format(team_abbrev, self.logo_variant)
-            )
+                
+        self.logo = ImageHelper.image_from_svg(
+            LOGO_URL.format(team_abbrev, self.logo_name)
+        )
 
         self.logo.thumbnail(self.get_size())
         self.logo.save(filename)
+
+    def change_ownership(self,team_abbrev):
+        path = os.path.dirname(PATH)
+        for root, dirs, files in os.walk(path):  
+            for d in dirs:  
+                os.chown(os.path.join(root, d), uid, gid)
+            for f in files:
+                os.chown(os.path.join(root, f), uid, gid)
+        print("changed owenership")
 
     def render(self):
         self.matrix.draw_image_layout(
