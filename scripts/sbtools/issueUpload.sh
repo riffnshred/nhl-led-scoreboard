a93e91acca2dbae4727cae4a5b017b829d5fda23
Sean Ostermann <falkyre@gmail.com>
2021-02-07 20:16:56 -0600

    Updated to new config.json.schema.  issueUpload.sh also will now take a parameter that is the name of the supervisor process the scoreboard runs under.  eg issueUpload.sh <name of scoreboard process>

--- scripts/sbtools/issueUpload.sh
+++ scripts/sbtools/issueUpload.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 # Upload the scoreboard stderr, stdout and config.json to pastebin using pastebinit
+if [ -n "${1}" ]; then
+   scoreboard_proc=$1
+else
+   scoreboard_proc="scoreboard"
+fi
 
 #Create temp file with the data
-ROOT=`dirname $(git rev-parse --git-dir)`
-version=`cat ${ROOT}/VERSION`
+ROOT=`/usr/bin/dirname $(/usr/bin/git rev-parse --git-dir)`
+version=`/bin/cat ${ROOT}/VERSION`
 currdate=`date`
 echo "nhl-led-scoreboard issue data ${currdate}" > /tmp/issue.txt
 echo "=============================" >>/tmp/issue.txt
 echo "" >> /tmp/issue.txt
 echo "Running V${version} on a " >> /tmp/issue.txt
-echo `neofetch --off --stdout | grep Host` >> /tmp/issue.txt
+echo `/usr/bin/neofetch --off --stdout | grep Host` >> /tmp/issue.txt
 echo "------------------------------------------------------" >> /tmp/issue.txt
 echo "config.json" >> /tmp/issue.txt
 echo "" >>/tmp/issue.txt
-jq '.boards.weather.owm_apikey=""' ${ROOT}/config/config.json >> /tmp/issue.txt
+/usr/bin/jq '.boards.weather.owm_apikey=""' ${ROOT}/config/config.json >> /tmp/issue.txt
 echo "" >> /tmp/issue.txt
 echo "------------------------------------------------------" >> /tmp/issue.txt
 echo "supervisorctl status" >> /tmp/issue.txt
 echo "------------------------------------------------------" >> /tmp/issue.txt
-supervisorctl status >>/tmp/issue.txt
+/usr/local/bin/supervisorctl status >>/tmp/issue.txt
 echo "------------------------------------------------------" >> /tmp/issue.txt
-echo "scoreboard stderr log, first 128kb" >> /tmp/issue.txt
+echo "${scoreboard_proc} stderr log, first 128kb" >> /tmp/issue.txt
 echo "=================================" >> /tmp/issue.txt
-supervisorctl tail -128000 scoreboard stderr >> /tmp/issue.txt
+/usr/local/bin/supervisorctl tail -128000 $scoreboard_proc stderr >> /tmp/issue.txt
 echo "" >> /tmp/issue.txt
 echo "------------------------------------------------------" >> /tmp/issue.txt
-echo "scoreboard stdout log, first 128kb" >> /tmp/issue.txt
+echo "${scoreboard_proc} stdout log, first 128kb" >> /tmp/issue.txt
 echo "=================================" >> /tmp/issue.txt
-supervisorctl tail -128000 scoreboard >> /tmp/issue.txt
-url=`pastebinit -b paste.ubuntu.com -t "nhl-led-scoreboard issue logs and config" < /tmp/issue.txt`
+/usr/local/bin/supervisorctl tail -128000 $scoreboard_proc >> /tmp/issue.txt
+url=`/usr/bin/pastebinit -b paste.ubuntu.com -t "nhl-led-scoreboard issue logs and config" < /tmp/issue.txt`
 echo "Take this url and paste it into your issue.  You can create an issue @ https://github.com/riffnshred/nhl-led-scoreboard/issues"
 echo "${url}"
-#rm /tmp/issue.txt
+rm /tmp/issue.txt
