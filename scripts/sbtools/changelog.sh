9ae25148811b0abc4bf870f9216f8b82efb89695
Sean Ostermann <falkyre@gmail.com>
2021-02-11 08:07:10 -0600

    updates to sb-upgrade and changelog to reflect nhl-led-scoreboard repo

--- scripts/sbtools/changelog.sh
+++ scripts/sbtools/changelog.sh
@@ -186,7 +186,7 @@ function display-release {
     case "$output" in
     raw) printf "$hash" ;;
     text) printf "\e[33m$hash\e[0m" ;; # red
-    md) printf "[\`$hash\`](https://github.com/ohmyzsh/ohmyzsh/commit/$hash)" ;;
+    md) printf "[\`$hash\`](https://github.com/riffnshred/nhl-led-scoreboard/commits/$hash)" ;;
     esac
   }
 
@@ -256,7 +256,7 @@ function display-release {
     # In text mode, highlight (#<issue>) and dim text between `backticks`
     text) sed -E $'s|#([0-9]+)|\e[32m#\\1\e[0m|g;s|`([^`]+)`|`\e[2m\\1\e[0m`|g' <<< "$subject" ;;
     # In markdown mode, link to (#<issue>) issues
-    md) sed -E 's|#([0-9]+)|[#\1](https://github.com/ohmyzsh/ohmyzsh/issues/\1)|g' <<< "$subject" ;;
+    md) sed -E 's|#([0-9]+)|[#\1](https://github.com/riffnshred/nhl-led-scoreboard/issues/\1)|g' <<< "$subject" ;;
     esac
   }
 
@@ -362,7 +362,7 @@ function main {
     # If $since is not specified:
     # 1) try to find the version used before updating
     # 2) try to find the first version tag before $until
-    since=$(command git config --get oh-my-zsh.lastVersion 2>/dev/null) || \
+    since=$(command git config --get nhl-led-scoreboard.lastVersion 2>/dev/null) || \
     since=$(command git describe --abbrev=0 --tags "$until^" 2>/dev/null) || \
     unset since
   elif [[ "$since" = --all ]]; then
