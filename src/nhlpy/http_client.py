31e84fec0a599a02ea2c609e5f6c4d89636dc063
azaroth08 <jay.jay0088@gmail.com>
2024-01-20 22:34:06 -0500

    added try/except for httpx timeout error to api calls

--- src/nhlpy/http_client.py
+++ src/nhlpy/http_client.py
@@ -8,14 +8,19 @@ class HttpClient:
             logging.basicConfig(level=logging.INFO)
 
     def get(self,resource: str) -> httpx.request:
-        r: httpx.request = httpx.get(
-                url=f"{self._config.api_web_base_url}{self._config.api_web_api_ver}{resource}", follow_redirects=True
-        )
+        try:
+            r: httpx.request = httpx.get(
+                    url=f"{self._config.api_web_base_url}{self._config.api_web_api_ver}{resource}", follow_redirects=True
+            )
 
-        if self._config.verbose:
-            logging.info(F"API URL: {r.url}")
+            if self._config.verbose:
+                logging.info(F"API URL: {r.url}")
 
-        return r
+            return r
+        except httpx.ReadTimeout as exc:
+            logging.info(f"Time out error as:\n {exc}")
+            exit()
+            
 
     def get_by_url(self, full_resource: str) -> httpx.request:
         r: httpx.request = httpx.get(url=full_resource, follow_redirects=True)
